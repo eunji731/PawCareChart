341710c11ebfacd9417a9fc20edfe48957bfebdb
이은지 <ohohdmswlgd@gmail.com>
2026-03-25 17:28:16 +0900

    back-fix : localhost가 아닌 ip로 변경(모바일에서 개발 확인 필요)

--- backend/src/main/java/com/pawcarechart/backend/config/WebConfig.java
+++ backend/src/main/java/com/pawcarechart/backend/config/WebConfig.java
@@ -9,7 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로에 대해
-                .allowedOrigins("http://localhost:5173") // 리액트(Vite) 포트 허용
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173",
+                        "http://192.168.10.70:5173"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
