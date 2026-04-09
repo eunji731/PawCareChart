c1a5173c8286946f11039619b2f023df3bc8b0a8
이은지 <ohohdmswlgd@gmail.com>
2026-04-09 15:33:30 +0900

    front, back-fix  : url 도메인으로 변경

--- backend/src/main/java/com/pawcarechart/backend/config/security/SecurityConfig.java
+++ backend/src/main/java/com/pawcarechart/backend/config/security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.10.70:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.10.70:5173","http://35.175.212.189", "https://pawcarechart.site"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-XSRF-TOKEN"));
         configuration.setAllowCredentials(true);
